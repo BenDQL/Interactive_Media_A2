3da1dbcaa4ff381065727bca5c2262dadee422b9
BenDQL <131518649+BenDQL@users.noreply.github.com>
2024-05-20 14:46:11 +1000

    update A2

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-/* PlayList Control Section */
-let currentIndex = 1;
-const carousel = document.querySelector(".carousel");
-const planetImg = document.querySelector(".planet");
-const playlistItems = document.querySelectorAll(".playlist span");
-const playlist = document.querySelector(".playlist");
-const playButton = document.querySelector(".play-btn");
-const mediaPlayer = document.querySelector(".media-player");
-
-const rotateValue = [-60, 0, 60];
-
-const images = ["planet1", "planet2", "planet3"];
-
-const rotate = (rotateValue) => {
-  planetImg.style.transform = `rotate(${rotateValue}deg)`;
-};
-
-playlistItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    planetImg.style.backgroundImage = `url(images/${images[index]}.svg)`;
-    rotate(rotateValue[index]);
-    // Rotate the name as well
-    playlist.style.transform = `translate(-50%, -50%) rotate(${-rotateValue[
-      index
-    ]}deg)`;
-
-    currentIndex = index;
-  });
-});
-
-const videoPlayControl = (showVideo) => {
-  if (showVideo) {
-    mediaPlayer.style.visibility = "visible";
-    mediaPlayer.style.transform = `scale(1.5)`;
-    carousel.style.transform = `translateX(-50%) translateY(20%)`;
-  } else {
-    mediaPlayer.style.visibility = "hideen";
-    mediaPlayer.style.transform = `scale(1)`;
-    carousel.style.transform = `translateX(-50%)`;
-  }
-};
-
-playButton.addEventListener("click", () => videoPlayControl(true));
-
 const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
@@ -50,6 +6,8 @@ const returnToStart = document.querySelector("#skip-to-start");
 const volumeBtn = document.querySelector("#volume-img");
 const fullScreen = document.querySelector("#full-screen");
 const nextVideo = document.querySelector("#next");
+const planetImg = document.querySelector("#planetBg");
+
 video.removeAttribute("controls");
 // playPauseBtn.addEventListener("click", togglePlayPause);
 video.addEventListener("timeupdate", updateProgressBar);
@@ -92,6 +50,61 @@ function screenSize() {
 function endMusic() {
   video.ended();
 }
+
+const playlistItems = document.querySelectorAll(".box .list");
+
+const videos = [
+  {
+    source:
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/berry-bunch_handheld-windmill-fan.mp4",
+    name: "Crafting Tutorial Video",
+    cover: "videoCover1",
+    description:
+      "Stay cool with the Berry Bunch Handheld Windmill Fan! Compact and cute with colorful blades, it's perfect for on-the-go cooling. Enjoy a refreshing breeze anywhere!",
+    bgImg: "planet1",
+  },
+  {
+    source:
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
+    name: "Video Art",
+    cover: "videoCover2",
+    description:
+      "Meet MiaC, your interactive digital pet! This colorful gadget responds to touch, sound, and motion with lights and adorable sounds, providing endless fun and companionship. Perfect for on-the-go joy!",
+
+    bgImg: "planet2",
+  },
+  {
+    source:
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/miac.mp4",
+    name: "Music Video",
+    cover: "videoCover3",
+    description:
+      "This vibrant gadget responds to touch, sound, and motion with colorful lights and playful sounds. Perfect for bringing joy and entertainment on the go!",
+
+    bgImg: "planet3",
+  },
+];
+
+setup = (currentIndex) => {
+  const videoDom = document.getElementById("custom-video-player");
+  videoDom.src = videos[currentIndex].source;
+  const videoName = document.getElementById("video-name");
+  videoName.textContent = videos[currentIndex].name;
+  const videoBrief = document.getElementById("video-brief");
+  videoBrief.textContent = videos[currentIndex].description;
+  planetImg.src = `images/${videos[currentIndex].bgImg}.svg`;
+};
+
+playlistItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    // Replace the main content
+    setup(index);
+
+    currentIndex = index;
+  });
+});
+
+setup(1);
 
 // --------------------
 
